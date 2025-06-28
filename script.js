f7a06bec9ff6abc7f6b4e73a34c32dd5408a58f9
gabriel. <gndibe1@gmail.com>
2025-06-28 13:42:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function calculateGrade() {
+ const input = document.getElementById("score-input");
+   const score = parseFloat(input.value);
+    const errorMessage = document.getElementById("errorMessage");
+    const resultDiv = document.getElementById('result');
+
+errorMessage.textContent= "";
+resultDiv.textContent= "";
+
+ if (isNaN(score) || score < 0 || score > 100 ) {
+    errorMessage.textContent = "Please enter a valid score between 0 and 100";
+   setTimeout(() => {
+    errorMessage.textContent= "";
+   }, 60000);
+    return;
+
+    
+ }
+ let grade;
+ if (score >= 90) {
+    grade = "A";
+ } else if (score >= 75) {
+   grade = "B";
+ } else if (score >= 50) {
+  grade = "C"
+} else if (score >= 40) {
+  grade ="D"
+} else {
+    grade = "E";
+}
+resultDiv.textContent = (`You got ${grade}`);
+input.value = "";
+}
